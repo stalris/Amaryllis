35e32d25208fb049cf2f3374ac2fbb50931d51f3
stalris <salc1990@gmail.com>
2024-03-16 01:38:07 -0400

    Didn't know where to submit this hw for csf101 so,
    Into Davy Jones' Locker you go! KEKW

---
+++ hw1.py
@@ -0,0 +1,53 @@
+What are your academic interests?
+I dont have any. 
+    The one defining characteristic that makes anything enjoyable is whether it is meaningful. I feel like any kind of work can be enjoyable so long as you
+find meaning in said work. For example, most people would find working at Mcdonalds dull and soul crushing, but would those same people find cooking for
+their children, their spouse, their families to also be a similar experience?
+import IHaveNoIdeaWhatImDoing.py
+I dont know how true this is but I sometimes see videos on youtube or posts on programming subreddits where people elaborate on their imposter syndrome.
+I would imagine that many people feel this way when they first start a new job, regardless of occupation.
+
+    The Department of Math, Engineering, 
+and Computer science lists Computer Technology, Network admin and information security, and programming and software development as related degrees offered
+here at Laguardia. The reason why I chose Computer Science was due to taking Harvards introductory course on Computer Science at edx.org. The professor who
+taught the course(He still teaches the course, but he used to back then too) was a guy called David Malan. He was very enthusiastic and very knowledgeable 
+about the subject and I enjoyed taking the course so much that I decided to look into how to become a programmer which eventually lead to my enrollment
+for computer science in York and later Laguardia. Network Administration seems to involve setting up computer networks and how to keep them safe. Laguardia
+describes the Computer Technology major as teaching students to "Understand computer system architecture and to work in areas where computer electronics are
+deployed, controlled, and maintained". While I enjoyed the process of building my own computers I dont think I would find the doing it for a living to be 
+enjoyable. The other program, Programming and Software Development AAS, is similar to computer science. It looks to have more classes where you can practice
+coding as opposed to Computer Science which has more math classes which(I am guessing) would help you understand the theory behind concepts used in 
+algorithms and data structures. 
+
+    Do I know enough about this major? I would say no because, realistically, no amount of research could paint an accurate enough picture to definitely 
+tell me whether I would enjoy or dislike a career in this field. Even getting a job as a programmer wouldnt be definitive because working at one company 
+could be wildly different than working at another, or even working for the goverment. The only realistic method would be to do the job myself and judge
+for myself whether the work is enjoyable, and if not enjoyable then at least judge whether I could tolerate it for the money. The requirements for the 
+degree seem to be 4 programming classes and just as many math classes. The careers available to this major are many, but mostly stem with designing and 
+implementing computer software. From what I remember, one of the many skills needed as a software developer is the ability to identify problems and to solve
+them with as little help as possible. It is one thing to be asked a specific question and expected to solve it, and another to be given a task which might 
+involve a lot of research before you can properly understand the scope of the problem, let alone solve it. As one user on /r/ExperiencedDevs puts it, "A 
+junior developer typically requires specific, concrete implementation instructions. Create a new endpoint with this name, it should accept these 
+parameters with these types, and the response should look like this. And here are the error conditions to watch out for." A slightly more experienced 
+developer is expected to "be able to take a high-level description of a solution and implement it, knowing when to ask for clarifications and what to 
+watch out for. Expose a new endpoint on top of this object turns into an end point with a sensible name, good error handling, and the correct parameters, 
+with the developer asking questions when they need to (what type should this parameter be?)" A senior developer should be able to "take a problem and come
+up with a solution that can then be broken down into implementation tasks. They should also be able to present multiple solutions and explain why we might
+choose one or the other (where applicable). They should be able to start working with non-technical people to clarify things and be able to turn technical
+questions into questions that non-technical people can understand. We need to expose this data to external consumers turns into we can either create an
+API or a data feed, heres why we would do one vs. the other. Also, you said that this field should be a number, I just want to clarify if it's always going
+to be a whole number or if decimals make sense from a business perspective? (a more junior developer might ask a non-technical user if the field should be 
+an int or float and that conversation won't go anywhere)." Another reddit user succintly describes the difference between developers as juniors needing 
+help, mid-levels as being independent, and seniors as providing help. 
+
+    The skills that I will learn that I think will be most helpful are the ability to network and to identify where to look for jobs and how to tailor my 
+(non-existent *cough*) skill set for any prospective employer. I dont know how true this is, but I remember hearing that any particular skill set isnt 
+important in the grand scheme of things because of how quickly new technologies emerge. Rather, what is important is the ability to adapt to an ever
+changing and fluid environment. Im too lazy(PepeLaugh) so here is a link to googles average compesation by levels 
+https://www.levels.fyi/companies/google/salaries/software-engineer?country=254. Where would I see myself in 5 years? Idk(KEKW). Hopefully I would be doing 
+something that I enjoy like swimming or playing video games, or reading books, or working out, etc. 
+
+sources:
+    https://knowyourmeme.com/memes/i-have-no-idea-what-im-doing
+    https://www.reddit.com/r/ExperiencedDevs/comments/wg708a/what_are_expectations_for_midlevel/
+    https://www.levels.fyi/companies/google/salaries/software-engineer?country=254
