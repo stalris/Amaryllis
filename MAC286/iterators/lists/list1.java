41d6fd5cdcfd5110e9c7b0400faa11397b24fe0d
stalris <salc1990@gmail.com>
2024-11-02 13:19:53 -0400

    하나 둘 셋 넷 다섯 여섯 일곱 여덟 아홉 열

--- MAC286/iterators/lists/list1.java
+++ MAC286/iterators/lists/list1.java
@@ -29,26 +29,52 @@ public class list1{
     return temp;
   }
 
-  // Add a number at index 'index' by shifting elements, starting the shifts at index. 
+  // Add a number at 'index' by shifting elements, starting the shifts at 'index'. 
   public void add(int n, int index){
-    // reject attemps to access an invalid index.
-    if(index <= -1 || index >= nums.length)
+    // reject attemps to access an invalid index. 
+    // index == size is a valid index. It means insert at the end of the list.
+    if(index < 0 || index > size)
       throw new IndexOutOfBoundsException();
     // Check if the array is full.
     if(size == nums.length)
       throw  new IllegalArgumentException();
-    // The end of the list is at index = size-1;
     int temp;
-    while(index <= size-1){
-      // swap n and nums[index]
-      temp = nums[index];
-      nums[index] = n;
+    int nums_ahead = size - index; // the number of elements between index (inclusive) and the end of the list(exclusive).
+    int swaps = nums_ahead + 1;  // If there are no elements ahead(index==size) then do at least one swap.
+
+    // Swaps n and nums[index+i], the first swap happens at index+0 = index. The last swap access the new end of the list.
+    for(int i = 0; i < swaps; i++){
+      temp = nums[index+i];
+      nums[index+i] = n;
       n = temp;
-    
-      // Carry over the last value of n into the next iteration. 
-      // This effectively becomes a bunch of element swaps, starting with the element at nums[index].
-      index++;
-    } 
+    }
+
     size++;
+  }
+
+  // remove and return the number at index. Shifts all subsequent elements to remove any dead space.
+  // Starts from the end of the list.
+  public int remove(int index){
+    // Check for invalid indices.
+    // Valid indices of a list are between [0, and size-1]
+    // You can write this as index >= 0 && index < size
+    // To indicate invalid indices, use demorgans to invert the expression.
+    // !(index >= 0 && index < size) =
+    // !(index >= 0) || !(index < size) = 
+    // index < 0 || index >= size
+    if(index < 0 || index >= size())
+      throw new IndexOutOfBoundsException();
+    int data = get(index);
+
+    // Start from the last element of the list, size-1.
+    int current_index = size - 1;
+    for(int i = 0, temp = 0, prev = 0; i < size; i++){
+      temp = nums[current_index-i]; // element at the current index.
+      nums[current_index-i] = prev; // replace with the TODO
+      prev = temp;
+    }
+
+    size--;
+    return data;
   }
 }
--- MAC286/iterators/test.java
+++ MAC286/iterators/test.java
@@ -3,11 +3,17 @@ import lists.list1;
 public class test{
   public static void main(String[] args){
     System.out.println("내 고양이 귀엽다");
-    list1 nums = new list1();
+    list1 nums = new list1(2);
     nums.add(9, 0);
     nums.add(8, 0);
     for(int i = 0; i < nums.size(); i++){
       System.out.println("\n\tnums["+i+"]: "+nums.get(i));
+    }
+
+    System.out.println("Removing all elements from the list, starting from index 0");
+    for(int i = 0, j = nums.size(); i < j; i++){
+      System.out.println("\n\tsize: "+nums.size());
+      System.out.println("\n\tRemoved: "+nums.remove(0));
     }
   }
 }
